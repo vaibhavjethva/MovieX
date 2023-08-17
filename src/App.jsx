744512482f8a0f2f1826e19eaa9776ecc246a9f2
Vaibhav Jethava <vaibhavjethava321@gmail.com>
2023-08-17 12:49:23 +0530

    error fixed

--- src/App.jsx
+++ src/App.jsx
@@ -22,6 +22,7 @@ function App() {
     useEffect(() => {
         fetchApiConfig();
         genresCall();
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
 
     const fetchApiConfig = () => {
