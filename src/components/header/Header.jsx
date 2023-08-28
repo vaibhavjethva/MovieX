f9bfa2ee9299a61017871b30ede960455fe7e3fa
Vaibhav Jethava <vaibhavjethava321@gmail.com>
2023-08-28 18:02:11 +0530

    error fixed

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -41,6 +41,7 @@ const Header = () => {
         return () => {
             window.removeEventListener("scroll", controlNavbar);
         };
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [lastScrollY]);
 
     const searchQueryHandler = (event) => {
